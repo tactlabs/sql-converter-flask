a68319f20512031efbcbbf7f6e830724cf63a007
rajacsp <raja.csp@gmail.com>
2018-08-23 21:36:41 -0400

    Base files added

---
+++ Main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+import os
+import sys
+from flask import request
+from random import randint
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    
+    sql  = request.form.get('sql')
+    to_sql  = request.form.get('to_sql')
+    
+    db = {
+        'sql' : sql,
+        'to_sql' : to_sql,
+        'result_sql' : convert_sql(sql),
+    }
+    
+    #return content
+    return render_template('result.html', db=db)
+
+def convert_sql(sql):
+    return sql
+
+if __name__ == '__main__':
+    host = os.environ.get('IP', '127.0.0.1')
+    port = int(os.environ.get('PORT', 5000))
+    
+    app.run(host= host, port = port, use_reloader = False)
+    
+    
+'''
+Sources:
+    http://www.compjour.org/lessons/flask-single-page/multiple-dynamic-routes-in-flask/
+    
+    https://www.learnpython.org/en/String_Formatting
+    
+    https://stackoverflow.com/questions/25888396/how-to-get-latitude-longitude-with-python
+    
+    https://github.com/googlemaps/google-maps-services-python
+    
+    AIzaSyCRhRz_mw_5wIGgF-I6PUy3js6dcY6zQ6Q
+    
+    Get Current Location:
+    https://stackoverflow.com/questions/44218836/python-flask-googlemaps-get-users-current-location-latitude-and-longitude
+    
+    Sample SQLs:
+    SELECT * FROM USER;
+'''
